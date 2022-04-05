141fd30e1a5ca623556e256e9448161bc2080100
Katherine Braught <braught2@illinois.edu>
2022-04-05 12:19:56 -0500

    working guards

--- pythonparser.py
+++ pythonparser.py
@@ -20,10 +20,6 @@ class Statement:
         self.modeType = modeType
         self.mode = mode
 
-    def __init__(self, code):
-        self.code = code
-        self.modeType = None
-        self.mode = None
     
     def print(self):
         print(self.code)
@@ -34,135 +30,81 @@ class Guard(Statement):
     def __init__(self, code, mode, modeType):
         super().__init__(code, mode, modeType)
 
-    def __init__(self, code):
-        super().__init__(code)
 
     def isModeCheck(self):
         return self.modeType != None
 
     def parseGuard(node, code):
-        #TODO parse out mode and modeType
-        return Guard(ast.get_source_segment(code, node.test))
+        #assume guard is a strict comparision (modeType == mode)
+        if isinstance(node.test, ast.Compare):
+            if ("mode" in str(node.test.left.id)):
+                modeType = str(node.test.left.id)
+                #TODO: get this to work
+                mode = str(node.test.comparators[0].attr)
+                return Guard(ast.get_source_segment(code, node.test), mode, modeType)
+        else:
+            return Guard(ast.get_source_segment(code, node.test), None, None)
         
 
 class Reset(Statement):
     def __init__(self, code, mode, modeType):
         super().__init__(code, mode, modeType)
 
-    def __init__(self, code):
-        super().__init__(code)
 
     def isModeUpdate(self):
         return self.modeType != None
 
     def parseReset(node, code):
-        #TODO parse out mode and modeType
-        return Reset(ast.get_source_segment(code, node))
+        #assume reset is modeType = newMode
+        print(str(node.targets[0]))
+        if ("mode" in str(node.targets[0].id)):
+            modeType = node.targets[0]
+            mode = node.value
+            return Reset(ast.get_source_segment(code, node), mode, modeType)
+        return Reset(ast.get_source_segment(code, node), None, None)
 
 def walktree(code, tree):
     vars = []
     out = []
+    mode_dict = {}
     for node in ast.walk(tree): #don't think we want to walk the whole thing because lose ordering/depth
+        if isinstance(node, ast.ClassDef):
+            if "Mode" in node.name:
+                modeType = str(node.name)
+                modes = []
+                for modeValue in node.body:
+                    modes.append(str(modeValue.targets[0].id))
+                mode_dict[modeType] = modes
         if isinstance(node, ast.FunctionDef):
             if node.name == 'controller':
                 #print(node.body)
                 out = parsenodelist(code, node.body, False, [])
-                #args = node.args
-                #for arg in args:
-                #        vars.append(arg.arg)
+                #print(type(node.args))
+                args = node.args.args
+                for arg in args:
+                    vars.append(arg.arg)
                         #todo: what to add for return values
-    print(vars)
-    return out
+    return [out, args, mode_dict]
 
-def parsenodelisttemp(code, nodes, addResets, pathsToMe):
-    childrens_guards=[]
-    childrens_resets=[]
-    results = []
-    found = []
-    #collected = []
-    newPathItems = []
-    pathsfromchild = []
-    for childnode in nodes:
-        if isinstance(childnode, ast.Assign) and addResets:
-            reset = Reset.parseReset(childnode, code)
-            print("found reset: " + reset.code)
-            childrens_resets.append(reset)
-            newPathItems.append(reset)
-        if isinstance(childnode, ast.If):
-            guard = Guard.parseGuard(childnode, code)
-            childrens_guards.append(guard)
-            tempnewPath = newPathItems.copy().append(guard)
-            print("found if statement: " + guard.code)
-            #parsenodelist returns a list of paths 
-
-            tempresults = parsenodelist(code, childnode.body, True, tempnewPath)
-            for result in tempresults:
-                pathsfromchild.append(result)
-        #TODO - can add support for elif and else
-        ''''
-    print("********")
-    print("Begin ending iteration:")
-    print("We have found this many items before: " + str(len(found)))
-    for item in found:
-        if isinstance(item, Statement):
-            print(item.code)
-        else:
-            print(item)
-    
-    print("And now we want to add these -----")
-    for item in childrens_guards:
-        print(item.code)
-    for item in childrens_resets:
-        print(item.code)
-    print("-------")
-'''
-    if len(found) == 0 and len(childrens_resets) > 0:
-        found.append([])
-    for item in found:
-        for reset in childrens_resets:
-        #for item in found:
-            item.append(reset)
-        results.append(item)
-        for guard in childrens_guards:
-            item.append(guard)
-    '''       
-    print("now we generated these results -----")
-    for result in results:
-        for item in result:
-            if isinstance(item, Statement):
-                print(item.code)
-            else:
-                print(item)
-                
-    print("----------")
-    print("********")
-    '''
-
-    for path in pathsToMe:
-        path.extend(newPathItems)
-    
-    return pathsToMe
 
 
 def parsenodelist(code, nodes, addResets, pathsToMe):
     childrens_guards=[]
     childrens_resets=[]
     recoutput = []
-    #ifstatement = []
+
     for childnode in nodes:
         if isinstance(childnode, ast.Assign) and addResets:
             reset = Reset.parseReset(childnode, code)
-            print("found reset: " + reset.code)
+            #print("found reset: " + reset.code)
             childrens_resets.append(reset)
         if isinstance(childnode, ast.If):
             guard = Guard.parseGuard(childnode, code)
             childrens_guards.append(guard)
-            print("found if statement: " + guard.code)
-            #parsenodelist returns a list of paths 
+            #print("found if statement: " + guard.code)
             tempresults = parsenodelist(code, childnode.body, True, [])
             for result in tempresults:
                 recoutput.append([result, guard])
-                #ifstatement.append(guard)
 
     
     pathsafterme = [] 
@@ -171,9 +113,9 @@ def parsenodelist(code, nodes, addResets, pathsToMe):
         pathsafterme.append(childrens_resets)
     else:
         for path,ifstatement in recoutput:
-            newpath = path.copy()#recoutput[index].copy()
+            newpath = path.copy()
             newpath.extend(childrens_resets)
-            newpath.append(ifstatement)#ifstatement[index])
+            newpath.append(ifstatement)
             pathsafterme.append(newpath)
             
     
@@ -203,36 +145,24 @@ if __name__ == "__main__":
     code = f.read()
     tree = ast.parse(code)
     #tree = ast.parse()
-    results = walktree(code, tree)
-    print("resultsssss:")
+    results, vars, modes = walktree(code, tree)
+
+    print("Paths found:")
     for result in results:
         for item in result:
             item.print()
+            #print(item.mode)
+            #print(item.modeType)
         print()
-    #print(results)
 
-    #a = Analyzer()
-    #a.visit(tree)
-    
-    #print(ast.dump(tree, indent=4))
-
-    #cfge = pycfg.PyCFGExtractor()
-
-
-    #for node in ast.walk(tree):
-    #    print(node.__class__.__name__)
-
-    #print("=== full AST ===")
-    #print(ast.dump(tree))
-
-    #print()
-    #print("=== visit ===")
-    #vis = BinOpVisitor()
-    #vis.visit(tree)
+    print("Modes found: ")
+    print(modes)
 
     output_dict.update(input_json)
+    
 
-    #file parsing set up
+    
+
    
     # #add edge, transition(guards) and resets
     #output_dict['edge'] = edges
@@ -241,10 +171,9 @@ if __name__ == "__main__":
     #output_dict['vertex'] = mode_list
 
     output_json = json.dumps(output_dict, indent=4)
-    #print(output_json)
     outfile = open(output_file_name, "w")
     outfile.write(output_json)
     outfile.close()
 
-    # print("wrote json to " + output_file_name)
+    print("wrote json to " + output_file_name)
 
