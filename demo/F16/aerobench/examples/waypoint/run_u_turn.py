da7d68e1b572451edeccd9bbaa74cb2cfefa840d
sayanmitracode <sayan.mitra@gmail.com>
2022-07-04 11:58:17 -0500

    moved ball, F16 init

---
+++ demo/F16/aerobench/examples/waypoint/run_u_turn.py
@@ -0,0 +1,81 @@
+'''
+Stanley Bak
+
+should match 'u_turn' scenario from matlab version
+'''
+
+import math
+
+from numpy import deg2rad
+import matplotlib.pyplot as plt
+
+from aerobench.run_f16_sim import run_f16_sim
+
+from aerobench.visualize import plot
+
+from waypoint_autopilot import WaypointAutopilot
+
+def main():
+    'main function'
+
+    ### Initial Conditions ###
+    power = 9 # engine power level (0-10)
+
+    # Default alpha & beta
+    alpha = deg2rad(2.1215) # Trim Angle of Attack (rad)
+    beta = 0                # Side slip angle (rad)
+
+    # Initial Attitude
+    alt = 1500        # altitude (ft)
+    vt = 540          # initial velocity (ft/sec)
+    phi = 0           # Roll angle from wings level (rad)
+    theta = 0         # Pitch angle from nose level (rad)
+    psi = 0           # Yaw angle from North (rad)
+
+    # Build Initial Condition Vectors
+    # state = [vt, alpha, beta, phi, theta, psi, P, Q, R, pn, pe, h, pow]
+    init = [vt, alpha, beta, phi, theta, psi, 0, 0, 0, 0, 0, alt, power]
+    tmax = 150 # simulation time
+
+    # make waypoint list
+    waypoints = [[-5000, -7500, alt],
+                 [-15000, -7500, alt],
+                 [-20000, 0, alt+500]]
+
+    ap = WaypointAutopilot(waypoints, stdout=True)
+
+    step = 1/30
+    extended_states = True
+    res = run_f16_sim(init, tmax, ap, step=step, extended_states=extended_states, integrator_str='rk45')
+    
+    print(f"Simulation Completed in {round(res['runtime'], 2)} seconds (extended_states={extended_states})")
+
+    plot.plot_single(res, 'alt', title='Altitude (ft)')
+    filename = 'alt.png'
+    plt.savefig(filename)
+    print(f"Made {filename}")
+
+    plot.plot_overhead(res, waypoints=waypoints)
+    filename = 'overhead.png'
+    plt.savefig(filename)
+    print(f"Made {filename}")
+
+    plot.plot_attitude(res)
+    filename = 'attitude.png'
+    plt.savefig(filename)
+    print(f"Made {filename}")
+
+    # plot inner loop controls + references
+    plot.plot_inner_loop(res)
+    filename = 'inner_loop.png'
+    plt.savefig(filename)
+    print(f"Made {filename}")
+
+    # plot outer loop controls + references
+    plot.plot_outer_loop(res)
+    filename = 'outer_loop.png'
+    plt.savefig(filename)
+    print(f"Made {filename}")
+
+if __name__ == '__main__':
+    main()
