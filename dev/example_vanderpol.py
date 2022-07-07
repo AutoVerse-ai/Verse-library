d3d9aa1f97a74a752460c724baefe79c43965a3a
Yangge Li <li213@illinois.edu>
2022-07-07 15:02:55 -0500

    trying a vanderpol model with the new algorithm with uncertain dynamics which doesn't work yet

---
+++ dev/example_vanderpol.py
@@ -0,0 +1,82 @@
+import numpy as np
+from scipy.integrate import ode
+import matplotlib.pyplot as plt 
+
+def dynamics1(t, state, u):
+    x1, x2 = state 
+    w1, w2 = u
+    x1_dot = w1*x2 
+    x2_dot = (1-x1**2)*x2*w2-x1
+    return [x1_dot, x2_dot]
+
+def dynamics2(t, state, u):
+    x1, x2, x1_hat, x2_hat = state 
+    w1, w2, w1_hat, w2_hat = u 
+    x1_dot = w1_hat*x2_hat+1.1*(x2-x2_hat)+2.3*(w1-w1_hat)
+    x2_dot = w2*x2-w2*x1_hat**2*x2-x1_hat+6.084*(x1-x1_hat) + 2.208*(w2-w2_hat)
+    x1_hat_dot = w1*x2+1.1*(x2_hat-x2)+2.3*(w1_hat-w1)
+    x2_hat_dot = w2_hat*x2_hat-w2_hat*x1**2*x2_hat-x1+6.084*(x1_hat-x1) + 2.208*(w2_hat-w2)
+    return [x1_dot, x2_dot, x1_hat_dot, x2_hat_dot]
+
+def TC_simulate1(mode, initialCondition, time_bound, time_step, noise=False):
+    time_bound = float(time_bound)
+    number_points = int(np.ceil(time_bound/time_step))
+    t = [i*time_step for i in range(0,number_points)]
+
+    init = initialCondition 
+    trace = [[0] + init]
+    for i in range(len(t)):
+        if noise:
+            w1 = np.random.uniform(0.9, 1.1)
+            w2 = np.random.uniform(0.9, 1.1)
+        else:
+            w1 = 1
+            w2 = 1
+        r = ode(dynamics1)
+        r.set_initial_value(init).set_f_params([w1,w2])
+        res:np.ndarray = r.integrate(r.t + time_step)
+        init = res.flatten().tolist()
+        trace.append([t[i] + time_step] + init) 
+    return trace 
+
+def TC_simulate2(mode, initialCondition, time_bound, time_step):
+    time_bound = float(time_bound)
+    number_points = int(np.ceil(time_bound/time_step))
+    t = [i*time_step for i in range(0,number_points)]
+
+    init = initialCondition 
+    trace = [[0] + init]
+    for i in range(len(t)):
+        w1, w1_hat = 0.9, 1.1
+        w2, w2_hat = 0.9, 1.1
+        r = ode(dynamics2)
+        r.set_initial_value(init).set_f_params([w1, w2, w1_hat, w2_hat])
+        res:np.ndarray = r.integrate(r.t + time_step)
+        init = res.flatten().tolist()
+        trace.append([t[i] + time_step] + init) 
+    return trace 
+
+if __name__ == "__main__":
+    for i in range(10):
+        res = TC_simulate1(
+            None, 
+            [1.4, 2.3],
+            6,
+            0.1,
+            True 
+        )
+        res = np.array(res)
+        plt.plot(res[:,0], res[:,1], 'b')
+
+    res = TC_simulate2(
+        None, 
+        [1.4, 2.3, 1.4, 2.3],
+        6,
+        0.1,
+    )
+
+    res = np.array(res)
+    plt.plot(res[:,0], res[:,1],'r')
+    plt.plot(res[:,0], res[:,3],'g')
+    
+    plt.show()
