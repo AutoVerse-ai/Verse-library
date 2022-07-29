ee0923070f239bedf6a098c0c8db1ae6e0be0598
lyg1597 <Lyg1597@gmail.com>
2022-07-28 22:25:02 -0500

    exploring linear transform

---
+++ dev/test4.py
@@ -0,0 +1,199 @@
+from typing import List
+
+from scipy.integrate import ode
+from scipy.optimize import minimize
+from sympy import Symbol, diff
+from sympy.utilities.lambdify import lambdify
+from sympy.core import *
+
+import numpy as np
+import matplotlib.pyplot as plt 
+
+def find_min(expr, var_range):
+    bounds = []
+    vars = list(expr.free_symbols)
+    x0 = []
+    jac = []
+    for var in vars:
+        bounds.append(var_range[var])
+        x0.append(var_range[var][0])
+        jac.append(diff(expr, var))
+    expr_func = lambdify([vars], expr)
+    jac_func = lambdify([vars], jac)
+    res = minimize(
+        expr_func, 
+        x0,
+        bounds = bounds,
+        jac = jac_func,
+        method = 'L-BFGS-B'
+    )
+    return res.fun
+
+def find_max(expr, var_range):
+    tmp = -expr 
+    res = find_min(tmp, var_range)
+    return -res
+
+def computeD(exprs, symbol_x, symbol_w, x, w, x_hat, w_hat):
+    d = []
+    for expr in exprs:
+        if all(a<=b for a,b in zip(x, x_hat)) and all(a<=b for a,b in zip(w,w_hat)):
+            var_range = {}
+            for i, var in enumerate(symbol_x):
+                var_range[var] = (x[i], x_hat[i])
+            for i, var in enumerate(symbol_w):
+                var_range[var] = (w[i], w_hat[i])
+            res = find_min(expr, var_range)
+            d.append(res)
+        elif all(a>=b for a,b in zip(x,x_hat)) and all(a>=b for a,b in zip(w,w_hat)):
+            var_range = {}
+            for i,var in enumerate(symbol_x):
+                var_range[var] = (x_hat[i], x[i])
+            for i, var in enumerate(symbol_w):
+                var_range[var] = (w_hat[i], w[i])
+            res = find_max(expr, var_range)
+            d.append(res)
+        else:
+            raise ValueError(f"Condition for x, w, x_hat, w_hat not satisfied: {[x, w, x_hat, w_hat]}")
+    return d
+
+def compute_reachtube(
+        initial_set: List[List[float]],
+        uncertain_var_bound: List[List[float]],
+        time_horizon: float,
+        time_step: float,
+        exprs: List[Expr],
+        vars: List[Symbol],
+        uncertain_vars: List[Symbol]
+    ):
+    '''
+    Example systems that we can handle x_{k+1} = F(x_k, w_k)
+    exprs: List[Expr]. The expressions defining function F
+    '''
+    assert len(uncertain_var_bound) == len(initial_set) == 2
+    assert len(uncertain_var_bound[0]) == len(uncertain_vars)
+    assert len(initial_set[0]) == len(vars)
+
+    number_points = int(np.ceil(time_horizon/time_step))
+    t = [round(i*time_step,10) for i in range(0,number_points)]
+
+    trace = [[0] + initial_set[0] + initial_set[1]]
+    num_var = len(vars)
+    num_uncertain_vars = len(uncertain_vars) 
+    for i in range(len(t)):
+        print(i)
+        xk = trace[-1]
+        x = xk[1:1+num_var]
+        x_hat = xk[1+num_var:]
+        w = uncertain_var_bound[0]
+        w_hat = uncertain_var_bound[1]
+        d = computeD(exprs, vars, uncertain_vars, x, w, x_hat, w_hat)
+        d_hat = computeD(exprs, vars, uncertain_vars, x_hat, w_hat, x, w)
+        trace.append([round(t[i]+time_step,10)]+d+d_hat)
+    return trace
+
+def reachtube_origin():
+    # Decomposition for the original system  
+    x1 = Symbol('x1',real=True)
+    x2 = Symbol('x2',real=True)
+    w1 = Symbol('w1', real=True)
+
+    time_horizon = 1.0
+    dt = 0.01
+    expr = [
+        x1 + dt*(x1-2*x2+x2**3+w1),
+        x2 + dt*(x1-2*x2),
+    ]
+    # expr_func = lambdify([(x1,x2,w1)], expr)
+    tube = compute_reachtube(
+        [[1,0], [1,0]],
+        [[-1],[1]],
+        time_horizon,
+        dt,
+        expr,
+        [x1,x2],
+        [w1]
+    )
+    tube = np.array(tube)
+
+    return tube
+
+def reachtube_transform1():
+    # Decomposition for the original system  
+    y1 = Symbol('y1',real=True)
+    y2 = Symbol('y2',real=True)
+    w1 = Symbol('w1', real=True)
+
+    time_horizon = 1.0
+    dt = 0.01
+    expr = [
+        y1 + dt*(y2**3+w1),
+        y2 + dt*(y1-y2),
+    ]
+    # expr_func = lambdify([(x1,x2,w1)], expr)
+    tube = compute_reachtube(
+        [[1,0], [1,0]],
+        [[-1],[1]],
+        time_horizon,
+        dt,
+        expr,
+        [y1,y2],
+        [w1]
+    )
+    tube = np.array(tube)
+    tube[:,1:3] = tube[:,1:3]@np.array([[1,1],[0,1]]).T
+    tube[:,3:5] = tube[:,3:5]@np.array([[1,1],[0,1]]).T
+    return tube
+
+def reachtube_transform2():
+    # Decomposition for the original system  
+    y1 = Symbol('y1',real=True)
+    y2 = Symbol('y2',real=True)
+    w1 = Symbol('w1', real=True)
+
+    time_horizon = 1.0
+    dt = 0.01
+    expr = [
+        y1 + dt*(-y1+y2**3+w1),
+        y2 + dt*(y1),
+    ]
+    # expr_func = lambdify([(x1,x2,w1)], expr)
+    tube = compute_reachtube(
+        [[1,0], [1,0]],
+        [[-1],[1]],
+        time_horizon,
+        dt,
+        expr,
+        [y1,y2],
+        [w1]
+    )
+    tube = np.array(tube)
+    tube[:,1:3] = tube[:,1:3]@np.array([[1,2],[0,1]]).T
+    tube[:,3:5] = tube[:,3:5]@np.array([[1,2],[0,1]]).T
+    return tube
+
+if __name__ == "__main__":
+    trace1 = reachtube_origin()
+    trace2 = reachtube_transform1()
+    trace3 = reachtube_transform2()
+
+    plt.figure(0)
+    plt.plot(trace1[:,0], trace1[:,1],'r')
+    plt.plot(trace2[:,0], trace2[:,1],'g')
+    plt.plot(trace3[:,0], trace3[:,1],'b')
+
+    plt.plot(trace1[:,0], trace1[:,1+2],'r')
+    plt.plot(trace2[:,0], trace2[:,1+2],'g')
+    plt.plot(trace3[:,0], trace3[:,1+2],'b')
+
+    plt.figure(1)
+    plt.plot(trace1[:,0], trace1[:,2],'r')
+    plt.plot(trace2[:,0], trace2[:,2],'g')
+    plt.plot(trace3[:,0], trace3[:,2],'b')
+
+    plt.plot(trace1[:,0], trace1[:,2+2],'r')
+    plt.plot(trace2[:,0], trace2[:,2+2],'g')
+    plt.plot(trace3[:,0], trace3[:,2+2],'b')
+
+    plt.show()
+
