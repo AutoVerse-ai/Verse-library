145741efda77fe870fea366c49fb132475c7093b
crides <zhuhaoqing@live.cn>
2023-02-18 19:53:45 -0600

    example: intersection: multi lane and switching

--- verse/map/example_map/intersection.py
+++ verse/map/example_map/intersection.py
@@ -1,4 +1,5 @@
-from typing import Dict
+from typing import Dict, Optional
+
 from verse.map.lane import Lane
 from verse.map.lane_map import LaneMap
 from verse.map.lane_segment import AbstractLane, CircularLane, StraightLane
@@ -12,29 +13,31 @@ class Intersection(LaneMap):
     width: width of each lane
     size: distance from center (of intersection) to when curves and straight lines meet
     length: length of straight road outside of intersection
+    lanes: number of lanes per direction (e.g. north and into the intersection)
     """
-    def __init__(self, width: float = 3, size: float = 20, length: float = 300):
-        assert width > 0 and length > 0
+    def __init__(self, width: float = 3, size: float = 20, length: float = 300, lanes: int = 2):
+        assert width > 0 and lanes > 0 and length > 0
+        assert size >= (1 + lanes) * width, "Intersection too small for the number of lanes"
         super().__init__()
-        self.width, self.size, self.length = width, size, length
-        off = width / 2
+        self.width, self.size, self.length, self.lanes = width, size, length, lanes
         outer_edge = size + length
-        segs: Dict[str, AbstractLane] = {
+        segs: Dict[str, AbstractLane] = {}
+        for i in range(lanes):
+            off = width * (i + 1/2)
             # Outer straights
-            "NR": StraightLane("NR", (-off, outer_edge), (-off, size), width),
-            "SR": StraightLane("SR", (off, -outer_edge), (off, -size), width),
-            "ER": StraightLane("ER", (outer_edge, off), (size, off), width),
-            "WR": StraightLane("WR", (-outer_edge, -off), (-size, -off), width),
-            "NL": StraightLane("NL", (off, size), (off, outer_edge), width),
-            "EL": StraightLane("EL", (size, -off), (outer_edge, -off), width),
-            "SL": StraightLane("SL", (-off, -size), (-off, -outer_edge), width),
-            "WL": StraightLane("WL", (-size, off), (-outer_edge, off), width),
+            segs[f"NR{i}"] = StraightLane(f"NR{i}", (-off, outer_edge), (-off, size), width)
+            segs[f"SR{i}"] = StraightLane(f"SR{i}", (off, -outer_edge), (off, -size), width)
+            segs[f"ER{i}"] = StraightLane(f"ER{i}", (outer_edge, off), (size, off), width)
+            segs[f"WR{i}"] = StraightLane(f"WR{i}", (-outer_edge, -off), (-size, -off), width)
+            segs[f"NL{i}"] = StraightLane(f"NL{i}", (off, size), (off, outer_edge), width)
+            segs[f"EL{i}"] = StraightLane(f"EL{i}", (size, -off), (outer_edge, -off), width)
+            segs[f"SL{i}"] = StraightLane(f"SL{i}", (-off, -size), (-off, -outer_edge), width)
+            segs[f"WL{i}"] = StraightLane(f"WL{i}", (-size, off), (-outer_edge, off), width)
             # Inner straights
-            "NSR": StraightLane("NSR", (-off, size), (-off, -size), width),
-            "NSL": StraightLane("NSL", (off, -size), (off, size), width),
-            "WER": StraightLane("WER", (-size, -off), (size, -off), width),
-            "WEL": StraightLane("WEL", (size, off), (-size, off), width),
-        }
+            segs[f"NSR{i}"] = StraightLane(f"NSR{i}", (-off, size), (-off, -size), width)
+            segs[f"NSL{i}"] = StraightLane(f"NSL{i}", (off, -size), (off, size), width)
+            segs[f"WER{i}"] = StraightLane(f"WER{i}", (-size, -off), (size, -off), width)
+            segs[f"WEL{i}"] = StraightLane(f"WEL{i}", (size, off), (-size, off), width)
         curve_params = {
             "NW": ((-size, size), 3),
             "NE": ((size, size), 2),
@@ -43,31 +46,52 @@ class Intersection(LaneMap):
         }
         segs.update(
             {
-                (n := k + "IO"[io]): CircularLane(
+                (n := k + "IO"[io] + str(lane)): CircularLane(
                     n,
                     center,
-                    size + width / 2 * [-1, 1][io],
+                    size + width * (lane + 1/2) * [-1, 1][io],
                     (start + (int(io == 0))) * math.pi / 2,     # XXX inner circulars are clockwise, thus start larger then end
                     (start + (int(io == 1))) * math.pi / 2,     # also python bool converts to int like C would
                     clockwise=io == 0,
                     width=width,
                 )
                 for k, (center, start) in curve_params.items()
-                for io in range(2)
+                for io in range(2) for lane in range(lanes)
             }
         )
         # straight
-        lanes = [
+        straights = [
             ("NS", "NR", "NSR", "SL"),
             ("SN", "SR", "NSL", "NL"),
             ("WE", "WR", "WER", "EL"),
             ("EW", "ER", "WEL", "WL"),
         ]
-        self.add_lanes([Lane(lane[0], [segs[seg] for seg in lane[1:]]) for lane in lanes])
+        self.add_lanes([Lane(f"{straight[0]}.{i}", [segs[seg + str(i)] for seg in straight[1:]]) for straight in straights for i in range(lanes)])
         # curve
         curves = ["NWI", "NEO", "WNO", "ENI", "SWO", "SEI", "WSI", "ESO"]
-        self.add_lanes([Lane(s + e, [segs[s + "R"], segs.get(s + e + d, None) or segs[e + s + d], segs[e + "L"]])
-                        for s, e, d in curves])
-        all_lanes = [straight[0] for straight in lanes] + [curve[:2] for curve in curves]
-        actions = ["Brake", "Accel"]
-        self.h_dict = {(lane, actions[i], actions[1 - i]): lane for lane in all_lanes for i in range(2)}
+        self.add_lanes([Lane(f"{s}{e}.{i}", [segs[s + "R" + str(i)], segs.get(s + e + d + str(i), None) or segs[e + s + d + str(i)], segs[e + "L" + str(i)]])
+                        for s, e, d in curves for i in range(lanes)])
+
+    def h(self, lane_idx: str, mode_src: str, mode_dest: str) -> Optional[str]:
+        # ret = self._h(lane_idx, mode_src, mode_dest)
+        # print("H", lane_idx, mode_src, mode_dest, "->", ret)
+        # return ret
+
+    # def _h(self, lane_idx: str, mode_src: str, mode_dest: str) -> Optional[str]:
+        src_sw, dst_sw = mode_src.startswith("Switch"), mode_dest.startswith("Switch")
+        if src_sw:
+            if dst_sw:
+                return None
+            else:
+                lanes, ind = lane_idx.split(".")
+                ind = int(ind)
+                if "Left" in mode_dest and ind > 0:
+                    return f"{lanes}.{ind - 1}"
+                if "Right" in mode_dest and ind < self.lanes - 1:
+                    return f"{lanes}.{ind + 1}"
+                return None
+        else:
+            if dst_sw:
+                return lane_idx
+            else:
+                return lane_idx
