27e29fab4df0cab06c28f9559b1b91aace70bcd9
keyis2 <keyis2@illinois.edu>
2023-03-07 23:07:18 +0800

    add exp, find issues in intersection and equality checker

--- exp.sh
+++ exp.sh
@@ -1,18 +1,21 @@
-rm -f exp.txt
+#/bin/bash
+
+para=''
+inc='i'
+
+file="exp_v${inc}${para}.txt"
+rm -f $file
+
 echo '=========================inc-exp-r============================'
-echo '=========================inc-exp-r============================' >> exp.txt
-python demo/tacas2023/exp11/inc-expr.py vril >> exp.txt
+echo '=========================inc-exp-r============================' >> $file
+python demo/tacas2023/exp11/inc-expr.py "vr${inc}${para}" >> $file
 # diff demo/tacas2023/exp1/output_par.json demo/tacas2023/exp1/output_ser.json >> diff.txt
 echo '=========================inc-exp-n============================'
-echo '=========================inc-exp-n============================' >> exp.txt
-python demo/tacas2023/exp11/inc-expr.py vnil >> exp.txt
+echo '=========================inc-exp-n============================' >> $file
+python demo/tacas2023/exp11/inc-expr.py "vn${inc}${para}" >> $file
 # diff demo/tacas2023/exp3/output3_par.json demo/tacas2023/exp3/output3_ser.json >> diff.txt
-echo '=========================inc-exp-3============================'
-echo '=========================inc-exp-3============================' >> exp.txt
-python demo/tacas2023/exp11/inc-expr.py v3il >> exp.txt
-# diff demo/tacas2023/exp5/output5_par.json demo/tacas2023/exp5/output5_ser.json >> diff.txt
 echo '=========================inc-exp-8============================'
-echo '=========================inc-exp-8============================' >> exp.txt
-python demo/tacas2023/exp11/inc-expr.py v8il >> exp.txt
+echo '=========================inc-exp-8============================' >> $file
+python demo/tacas2023/exp11/inc-expr.py "v8${inc}${para}" >> $file
 # diff demo/tacas2023/exp9/output9_dryvr_par.json demo/tacas2023/exp9/output9_dryvr_ser.json >> diff.txt
 echo '=========================done============================'
