72731ebf0afc6c2c399fab225233c473250efb69
keyis2 <keyis2@illinois.edu>
2023-04-10 03:43:56 +0800

    simu issue, repeated transition sometimes.

--- demo/vehicle/intersection.py
+++ demo/vehicle/intersection.py
@@ -1,5 +1,5 @@
 import functools, pprint, random, math
-from verse.agents.example_agent import CarAgent
+from verse.agents.example_agent import CarAgentSwitch2
 from verse.analysis.utils import wrap_to_pi
 from verse.map.example_map.intersection import Intersection
 from verse.scenario.scenario import Benchmark
@@ -7,7 +7,7 @@ pp = functools.partial(pprint.pprint, compact=True, width=130)
 
 from controller.intersection_car import AgentMode
 
-CAR_NUM = 9
+CAR_NUM = 5
 CAR_ACCEL_RANGE = (0.7, 3)
 CAR_SPEED_RANGE = (1, 3)
 CAR_THETA_RANGE = (-0.1, 0.1)
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     map = Intersection(lanes=3)
     bench.scenario.set_map(map)
     for i in range(CAR_NUM):
-        car = CarAgent(f"car{i}", file_name=ctlr_src, speed=rand(*CAR_SPEED_RANGE), accel=rand(*CAR_ACCEL_RANGE))
+        car = CarAgentSwitch2(f"car{i}", file_name=ctlr_src, speed=rand(*CAR_SPEED_RANGE), accel=rand(*CAR_ACCEL_RANGE))
         # if i == 0:
         #     for p in car.decision_logic.paths:
         #         print(ast_dump(p.val_veri), "<=", ast_dump(p.cond_veri))
@@ -68,7 +68,7 @@ if __name__ == "__main__":
         lane = random.randint(0, map.lanes - 1)
         start, off = map.size + rand(0, map.length * 0.3), rand(0, map.width) + map.width * lane
         pos = { "N": (-off, start), "S": (off, -start), "W": (-start, -off), "E": (start, off) }[src]
-        init = [*pos, wrap_to_pi(dir * math.pi / 2 + rand(*CAR_THETA_RANGE)), rand(*CAR_SPEED_RANGE)]
+        init = [*pos, wrap_to_pi(dir * math.pi / 2 + rand(*CAR_THETA_RANGE)), rand(*CAR_SPEED_RANGE), 0]
         modes = (AgentMode.Accel, f"{src}{dst}_{lane}")
         bench.scenario.set_init_single(car.id, (init,), modes)
 
