9acb734fa37dab7e8571c7f9367f34c934c3226a
crides <zhuhaoqing@live.cn>
2023-04-23 12:25:33 -0500

    fix intersection example

--- demo/vehicle/intersection.py
+++ demo/vehicle/intersection.py
@@ -1,4 +1,5 @@
 import functools, pprint, random, math
+from typing import Optional, Tuple
 from verse.agents.example_agent import CarAgentDebounced
 from verse.analysis.utils import wrap_to_pi
 from verse.map.example_map.intersection import Intersection
@@ -7,7 +8,8 @@ pp = functools.partial(pprint.pprint, compact=True, width=130)
 
 from controller.intersection_car import AgentMode
 
-CAR_NUM = 9
+JERK = 0.05
+CAR_NUM = 6
 LANES = 3
 CAR_ACCEL_RANGE = (0.7, 3)
 CAR_SPEED_RANGE = (1, 3)
@@ -22,14 +24,12 @@ def run(meas=False):
     else:
         bench.scenario.verifier.tube_cache_hits = (0, 0)
         bench.scenario.verifier.trans_cache_hits = (0, 0)
-    traces = bench.run(60, 0.05)
+    traces = bench.run(30, 0.05)
 
     if bench.config.dump:
-        traces.dump_tree()
-        traces.dump("main.json") 
         traces.dump("tree2.json" if meas else "tree1.json") 
 
-    if bench.config.plot and meas:
+    if bench.config.plot:
         import plotly.graph_objects as go
         from verse.plotter.plotter2D import reachtube_tree, simulation_tree
 
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     bench = Benchmark(sys.argv)
     ctlr_src = "demo/vehicle/controller/intersection_car.py"
     import time
-    if len(sys.argv) == 3:
+    if len(sys.argv) > 2:
         seed = int(sys.argv[2])
     else:
         seed = int(time.time())
@@ -56,22 +56,31 @@ if __name__ == "__main__":
     random.seed(seed)
 
     dirs = "WSEN"
-    map = Intersection(lanes=LANES)
+    map = Intersection(lanes=LANES, length=400)
     bench.scenario.set_map(map)
-    def set_init(id: str):
-        dir = random.randint(0, 3)
+    def set_init(id: str, alt_pos: Optional[Tuple[float, float]] = None):
+        dir = random.randint(0, 2)
         src = dirs[dir]
         dst_dirs = list(dirs)
         dst_dirs.remove(src)
         dst = dst_dirs[random.randint(0, 2)]
-        lane = random.randint(0, map.lanes - 1)
-        start, off = map.size + rand(0, map.length * 0.3), rand(0, map.width) + map.width * lane
-        pos = { "N": (-off, start), "S": (off, -start), "W": (-start, -off), "E": (start, off) }[src]
-        init = [*pos, *((wrap_to_pi(dir * math.pi / 2 + rand(*CAR_THETA_RANGE)), rand(*CAR_SPEED_RANGE)) if alt_pos == None else bench.scenario.init_dict[id][2:4]), 0]
-        modes = (AgentMode.Accel, f"{src}{dst}_{lane}")
-        bench.scenario.set_init_single(id, (init,), modes)
+        mid_lane_ind = int(map.lanes / 2 - 1)
+        lane = random.randint(mid_lane_ind, mid_lane_ind + 1)
+        start, off = (map.size + rand(0, map.length * 0.1), rand(0, map.width) + map.width * lane)
+        pos = { "N": (-off, start), "S": (off, -start), "W": (-start, -off), "E": (start, off) }[src] if alt_pos == None else alt_pos
+        init = [*pos, *((wrap_to_pi(dir * math.pi / 2 + rand(*CAR_THETA_RANGE)), rand(*CAR_SPEED_RANGE)) if alt_pos == None else bench.scenario.init_dict[id][0][2:4]), 0]
+        assert len(init) == 5, bench.scenario.init_dict
+        modes = (AgentMode.Accel, f"{src}{dst}_{lane}") if alt_pos == None else bench.scenario.init_mode_dict[id]
+        if not bench.config.sim:
+            def j(st, s):
+                return [st[0] + s * JERK, st[1] + s * JERK, *st[2:]]
+            bench.scenario.set_init_single(id, (j(init, -1), j(init, 1)), modes)
+        else:
+            bench.scenario.set_init_single(id, (init,), modes)
     car_id = lambda i: f"car{i}"
 
+    LANES = int(sys.argv[3])
+    CAR_NUM = int(sys.argv[4])
     for i in range(CAR_NUM):
         car = CarAgentDebounced(car_id(i), file_name=ctlr_src, speed=rand(*CAR_SPEED_RANGE), accel=rand(*CAR_ACCEL_RANGE))
         bench.scenario.add_agent(car)
@@ -84,10 +93,12 @@ if __name__ == "__main__":
         run(True)
     elif 'n' in bench.config.args:
         run()
-        set_init(car_id(3))
+        set_init(car_id(3), (100, -0.8))
         run(True)
     elif '3' in bench.config.args:
         run()
-        bench.scenario.agent_dict["car3"] = CarAgentDebounced('car3', file_name=ctlr_src.replace(".py", "_sw5.py"))
+        old_agent = bench.scenario.agent_dict["car3"]
+        bench.scenario.agent_dict["car3"] = CarAgentDebounced('car3', file_name=ctlr_src.replace(".py", "_sw5.py"),
+                                                              speed=old_agent.speed, accel=old_agent.accel)
         run(True)
     print("seed:", seed)
