204a5854feb6b9ab596723bada1b1982036321a8
crides <zhuhaoqing@live.cn>
2023-04-26 21:14:08 -0500

    narrower init

--- demo/vehicle/intersection.py
+++ demo/vehicle/intersection.py
@@ -8,7 +8,8 @@ pp = functools.partial(pprint.pprint, compact=True, width=130)
 
 from controller.intersection_car import AgentMode
 
-CAR_NUM = 5
+JERK = 0.05
+CAR_NUM = 6
 LANES = 3
 CAR_ACCEL_RANGE = (0.7, 3)
 CAR_SPEED_RANGE = (1, 3)
@@ -47,7 +48,7 @@ if __name__ == "__main__":
     bench = Benchmark(sys.argv)
     ctlr_src = "demo/vehicle/controller/intersection_car.py"
     import time
-    if len(sys.argv) == 3:
+    if len(sys.argv) > 2:
         seed = int(sys.argv[2])
     else:
         seed = int(time.time())
@@ -58,21 +59,28 @@ if __name__ == "__main__":
     map = Intersection(lanes=LANES, length=400)
     bench.scenario.set_map(map)
     def set_init(id: str, alt_pos: Optional[Tuple[float, float]] = None):
-        dir = random.randint(0, 3)
+        dir = random.randint(0, 2)
         src = dirs[dir]
         dst_dirs = list(dirs)
         dst_dirs.remove(src)
         dst = dst_dirs[random.randint(0, 2)]
         mid_lane_ind = int(map.lanes / 2 - 1)
         lane = random.randint(mid_lane_ind, mid_lane_ind + 1)
-        start, off = (map.size + rand(0, map.length * 0.3), rand(0, map.width) + map.width * lane)
+        start, off = (map.size + rand(0, map.length * 0.1), rand(0, map.width) + map.width * lane)
         pos = { "N": (-off, start), "S": (off, -start), "W": (-start, -off), "E": (start, off) }[src] if alt_pos == None else alt_pos
         init = [*pos, *((wrap_to_pi(dir * math.pi / 2 + rand(*CAR_THETA_RANGE)), rand(*CAR_SPEED_RANGE)) if alt_pos == None else bench.scenario.init_dict[id][0][2:4]), 0]
         assert len(init) == 5, bench.scenario.init_dict
         modes = (AgentMode.Accel, f"{src}{dst}_{lane}") if alt_pos == None else bench.scenario.init_mode_dict[id]
-        bench.scenario.set_init_single(id, (init,), modes)
+        if not bench.config.sim:
+            def j(st, s):
+                return [st[0] + s * JERK, st[1] + s * JERK, *st[2:]]
+            bench.scenario.set_init_single(id, (j(init, -1), j(init, 1)), modes)
+        else:
+            bench.scenario.set_init_single(id, (init,), modes)
     car_id = lambda i: f"car{i}"
 
+    LANES = int(sys.argv[3])
+    CAR_NUM = int(sys.argv[4])
     for i in range(CAR_NUM):
         car = CarAgentDebounced(car_id(i), file_name=ctlr_src, speed=rand(*CAR_SPEED_RANGE), accel=rand(*CAR_ACCEL_RANGE))
         bench.scenario.add_agent(car)
