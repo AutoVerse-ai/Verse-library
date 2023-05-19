048c9666fdb276e3a20cb9b8ea305ca374cd2556
crides <zhuhaoqing@live.cn>
2023-05-19 16:52:22 -0500

    scenario: export `ScenarioConfig`

--- verse/__init__.py
+++ verse/__init__.py
@@ -2,6 +2,6 @@ from verse import agents, sensor, scenario, plotter, map, parser, automaton, ana
 from verse.agents import BaseAgent
 from verse.sensor import BaseSensor
 from verse.map import LaneSegment, LaneMap, Lane
-from verse.scenario import Scenario
+from verse.scenario import Scenario, ScenarioConfig
 
 from verse.plotter import plotter2D, plotter3D
