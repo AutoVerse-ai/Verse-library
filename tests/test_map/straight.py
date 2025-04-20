6fefde2216e583617e99bcacbb31967867d2e516
Bach Hoang <bachh2@illinois.edu>
2025-04-20 17:39:21 -0500

    Add test for StraightLane

--- tests/test_map/straight.py
+++ tests/test_map/straight.py
@@ -1,5 +1,5 @@
 import unittest
-from verse.map import StraightLane, CircularLane
+from verse.map import StraightLane
 import numpy as np
 class testStraight(unittest.TestCase):
     def testPositionSimple(self):
@@ -10,11 +10,6 @@ class testStraight(unittest.TestCase):
         self.assertTrue(map.length != 0)
         self.assertTrue(list(map.start.shape) == list(map.end.shape))
         self.assertTrue(list(map.direction.shape) == list(map.start.shape))
-
-class testCircular(unittest.TestCase):
-    def testPositionSimple(self):
-        map = CircularLane("simple", center = [1, 2, 3, 4], radius=1.0, start_phase = np.pi, end_phase = 0)
-
 
 if __name__ == "__main__":
     unittest.main()
