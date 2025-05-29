f172206cacbdbafd654fe0818c2be26b627ae748
Bach Hoang <bachh2@illinois.edu>
2025-05-29 17:29:08 -0500

    remove comments since problems have been fixed

--- tests/test_sensor/sensor_sense.py
+++ tests/test_sensor/sensor_sense.py
@@ -1,6 +1,4 @@
 import unittest
 from verse.sensor import BaseSensor
 
-# TODO: Need some discussion about how to effectively test the sense function in the base_sensor.py code in sensor directory
-# As there are some helper functions (add_2d_states) return errors when accessing the agent as in the sense function.
-# it would be better to have further discussion on how to test this function effectively
+# TODO: Need some discussion about how to effectively test the sense function in the base_sensor.py code in sensor director
