a5718266a25feb9402e32b3634004d41c740ac12
Bach Hoang <bachh2@illinois.edu>
2025-05-29 13:22:41 -0500

    Add more test for sensors

--- tests/test_sensor/basic.py
+++ tests/test_sensor/basic.py
@@ -19,7 +19,7 @@ def initialize_agents_examples(mode : str) -> Dict[str, BaseAgent]:
         script_dir = os.path.realpath(os.path.dirname(__file__))
         input_code_name = os.path.join(script_dir, "../test_controller/example_controller5.py")
         car1 = CarAgent("car1", file_name=input_code_name)
-        car2 = NPCAgent("car2")
+        car2 = CarAgent("car2", file_name=input_code_name)
         return {"car1":car1, "car2":car2}
     return
 
@@ -41,38 +41,124 @@ class TestSensorBasic(unittest.TestCase):
         adds(d, "car", ["color", "speed"], ["red", 100])
         adds(d, "car", ["speed"], [120])
         self.assertTrue(list(d.values()) == [["red"], [100, 120]])
-    
-    # NOTE: This test case returns errors
-    # Here is the log of the error
-    #   Traceback (most recent call last):
-    #   File "/Users/bachhoang/Verse-library/tests/test_sensor/basic.py", line 55, in test_set_states_2d_one_agent
-    #     set_states_2d(disc, cont, thing, agent, cont_var, disc_var, stat_var)
-    #   File "/Users/bachhoang/miniconda3/envs/verse_env/lib/python3.11/site-packages/verse/sensor/base_sensor.py", line 18, in set_states_2d
-    #     state, mode, static = val
-    #     ^^^^^^^^^^^^^^^^^^^
-    # TypeError: cannot unpack non-iterable CarAgent object
-    # def test_set_states_2d_one_agent(self):
-    #     disc = {}
-    #     cont = {}
-    #     state_dict = initialize_agents_examples("mono")
-    #     agent = state_dict["car"]
-    #     args = agent.decision_logic.args[0]
-    #     arg_type = args.typ
-    #     thing = args.name
-    #     cont_var = agent.decision_logic.state_defs[arg_type].cont
-    #     disc_var = agent.decision_logic.state_defs[arg_type].disc
-    #     stat_var = agent.decision_logic.state_defs[arg_type].static
-    #     set_states_2d(disc, cont, thing, agent, cont_var, disc_var, stat_var)
-    #     print("disc is :", disc)
-    #     print("cont is :", cont)
-    #     self.assertTrue(True)
-    # def test_set_states_2d_multi_agent(self):
-    #     disc = {}
-    #     cont = {}
-    #     state_dict = initialize_agents_examples("multi")
-    #     self.assertTrue(True)
+    def test_set_states_2d_one_agent(self):
+        disc = {}
+        cont = {}
+        state_dict = initialize_agents_examples("mono")
+        agent = state_dict["car"]
+        args = agent.decision_logic.args[0]
+        arg_type = args.typ
+        thing = args.name
+        cont_var = agent.decision_logic.state_defs[arg_type].cont
+        disc_var = agent.decision_logic.state_defs[arg_type].disc
+        stat_var = agent.decision_logic.state_defs[arg_type].static
+        val = (
+            [0.0, 1.0, 2.0, 3.0],
+            ["up", "down", "left", "right"],
+            ["a", "b", "c", "d"]
+        )
+        print(cont_var, disc_var, stat_var)
+        set_states_2d(disc, cont, thing, val, cont_var, disc_var, stat_var)
+        print("disc is :", disc)
+        print("cont is :", cont)
+        self.assertTrue(True)
+    def test_set_states_2d_multi_agent(self):
+        disc = {}
+        cont = {}
+        state_dict = initialize_agents_examples("multi")
 
+        agent_1 = state_dict["car1"]
+        args_1 = agent_1.decision_logic.args[0]
+        arg_type_1 = args_1.typ
+        thing_1 = args_1.name
+        cont_var_1 = agent_1.decision_logic.state_defs[arg_type_1].cont
+        disc_var_1 = agent_1.decision_logic.state_defs[arg_type_1].disc
+        stat_var_1 = agent_1.decision_logic.state_defs[arg_type_1].static
+        val_1 = (
+            [0.0, 1.0, 2.0, 3.0],
+            ["up", "down", "left", "right"],
+            ["a", "b", "c", "d"]
+        )
 
+        agent_2 = state_dict["car2"]
+        args_2 = agent_2.decision_logic.args[0]
+        arg_type_2 = args_2.typ
+        thing_2 = args_2.name
+        cont_var_2 = agent_2.decision_logic.state_defs[arg_type_2].cont
+        disc_var_2 = agent_2.decision_logic.state_defs[arg_type_2].disc
+        stat_var_2 = agent_2.decision_logic.state_defs[arg_type_2].static
+        val_2 = (
+            [0.0, 1.0, 2.0, 3.0],
+            ["up", "down", "left", "right"],
+            ["a", "b", "c", "d"]
+        )
+        set_states_2d(disc, cont, thing_1, val_1, cont_var_1, disc_var_1, stat_var_1)
+        set_states_2d(disc, cont, thing_2, val_2, cont_var_2, disc_var_2, stat_var_2)
+
+        print("disc is :", disc)
+        print("cont is :", cont)
+
+        self.assertTrue(True)
+
+    def test_add_states_2d_one_agent(self):
+        disc = {}
+        cont = {}
+        state_dict = initialize_agents_examples("mono")
+        agent = state_dict["car"]
+        args = agent.decision_logic.args[0]
+        arg_type = args.typ
+        thing = args.name
+        cont_var = agent.decision_logic.state_defs[arg_type].cont
+        disc_var = agent.decision_logic.state_defs[arg_type].disc
+        stat_var = agent.decision_logic.state_defs[arg_type].static
+        val = (
+            [0.0, 1.0, 2.0, 3.0],
+            ["up", "down", "left", "right"],
+            ["a", "b", "c", "d"]
+        )
+        print(cont_var, disc_var, stat_var)
+        add_states_2d(disc, cont, thing, val, cont_var, disc_var, stat_var)
+        print("test_add_states_2d_one_agent disc is :", disc)
+        print("test_add_states_2d_one_agent cont is :", cont)
+        self.assertTrue(True)
+
+    def test_add_states_2d_multi_agent(self):
+        disc = {}
+        cont = {}
+        state_dict = initialize_agents_examples("multi")
+
+        agent_1 = state_dict["car1"]
+        args_1 = agent_1.decision_logic.args[0]
+        arg_type_1 = args_1.typ
+        thing_1 = args_1.name
+        cont_var_1 = agent_1.decision_logic.state_defs[arg_type_1].cont
+        disc_var_1 = agent_1.decision_logic.state_defs[arg_type_1].disc
+        stat_var_1 = agent_1.decision_logic.state_defs[arg_type_1].static
+        val_1 = (
+            [0.0, 1.0, 2.0, 3.0],
+            ["up", "down", "left", "right"],
+            ["a", "b", "c", "d"]
+        )
+
+        agent_2 = state_dict["car2"]
+        args_2 = agent_2.decision_logic.args[0]
+        arg_type_2 = args_2.typ
+        thing_2 = args_2.name
+        cont_var_2 = agent_2.decision_logic.state_defs[arg_type_2].cont
+        disc_var_2 = agent_2.decision_logic.state_defs[arg_type_2].disc
+        stat_var_2 = agent_2.decision_logic.state_defs[arg_type_2].static
+        val_2 = (
+            [0.0, 1.0, 2.0, 3.0],
+            ["up", "down", "left", "right"],
+            ["a", "b", "c", "d"]
+        )
+        add_states_2d(disc, cont, thing_1, val_1, cont_var_1, disc_var_1, stat_var_1)
+        add_states_2d(disc, cont, thing_2, val_2, cont_var_2, disc_var_2, stat_var_2)
+
+        print("disc is :", disc)
+        print("cont is :", cont)
+
+        self.assertTrue(True)
 
 
 if __name__ == "__main__":
