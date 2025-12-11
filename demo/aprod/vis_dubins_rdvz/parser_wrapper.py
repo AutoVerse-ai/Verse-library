dfb6b387356d2b6ca68c69441c2ed8e7294d3239
Alex Yuan <alexyuan0.ay@gmail.com>
2025-12-11 12:43:48 -0600

    fixed args order for non-CROWN function (doesn't matter for CROWN functions) and added way to handle map functions

--- demo/aprod/vis_dubins_rdvz/parser_wrapper.py
+++ demo/aprod/vis_dubins_rdvz/parser_wrapper.py
@@ -9,9 +9,11 @@ from sensor_parser import parsed_sensor_expr
 from wrapper_consts import ALIASES, ANGULAR_FUNCTIONS, MAP_FUNCTIONS
 # ,FUNC_DICT
 from prox_error_all_bounds import angular_span_rect_parser, combine_angular_bounds
+from bounded_map import get_heading_bounds_optimized, get_lateral_distance_bounds_optimized
 import itertools
 
-def parse_function(func: Callable, input_bounds: Dict[str, List[Tuple[float, float]]], piecewise_functions: List[Callable] = None):
+
+def parse_function(func: Callable, input_bounds: Dict[str, List[Tuple[float, float]]], piecewise_functions: List[Callable] = None, track_mode = None, track_map = None):
     """
     Process a function line-by-line, computing bounds at each step using parsed_sensor.
     
@@ -55,7 +57,9 @@ def parse_function(func: Callable, input_bounds: Dict[str, List[Tuple[float, flo
                 rhs_code,
                 list(current_bounds.keys()),
                 current_bounds,
-                piecewise_functions
+                piecewise_functions,
+                track_mode,
+                track_map
             )
             
             # Update variable bounds
@@ -75,7 +79,9 @@ def compute_expression_bounds_with_parsed_sensor(
     expr_code: str,
     arg_names: List[str],
     current_bounds: Dict[str, List[Tuple[float, float]]],
-    piecewise_functions: List[Callable]
+    piecewise_functions: List[Callable],
+    track_mode,
+    track_map
 ) -> Tuple[float, float]:
     """
     Use parsed_sensor to compute bounds for an expression.
@@ -89,8 +95,14 @@ def compute_expression_bounds_with_parsed_sensor(
         (lower_bound, upper_bound) tuple
 
     NOTE: have not handled way to handle angular variables that could potentially have 2 bounds instead of 1
-    NOTE cont: the ideal way to handle this is to compute all combinations of bounds, then if the function if the function is angular, simply apply the angular bound combine function to get minimal representation
-    NOTE cont: and non-angular functions should just combine all outputs to a single bound     
+        the ideal way to handle this is to compute all combinations of bounds, then if the function if the function is angular, simply apply the angular bound combine function to get minimal representation
+        and non-angular functions should just combine all outputs to a single bound     
+    
+    NOTE: should I let piecewise functions take mode as an input?
+
+    FIXME: input args are not ordered properly -- should be ordered according to when they show up in the function
+        this is an issue with ALL functions, although CROWN is likely already handled properly since we define the temp function args ourselves
+    NOTE: I think I fixed this, for example, w = atan2(y,x) != m = atan2(x,y) given x = [-1, -0.5], y = [-1,0.5], but further testing needed
     """
     
     for alias, func_name in ALIASES.items(): # NOTE: may want to optimize this in the future
@@ -98,11 +110,23 @@ def compute_expression_bounds_with_parsed_sensor(
             # Replace the alias with the actual function name
             expr_code = expr_code.replace(alias, func_name)
 
+    # NOTE: extract args from function in the correct order
+    tree = ast.parse(expr_code)
+    func_def = tree.body[0].value
+
+    # Extract the arguments from the func_code 
+    # NOTE: this works in all cases except CROWN, where we expect general expressions, not function calls
+    args = []
+    for node in ast.walk(func_def):
+        if isinstance(node, ast.Call):
+            args = [arg.id for arg in node.args if isinstance(arg, ast.Name) and arg.id in arg_names]
+            break
+
     # TODO: fix way args are being added, right now, it's pretty naive and substrings of function calls could potentially overlap with variable names
     if any(func in expr_code for func in ANGULAR_FUNCTIONS):
         # Handle angular functions
         func_name = [func for func in ANGULAR_FUNCTIONS if func in expr_code][0]
-        args = [arg for arg in current_bounds.keys() if arg in expr_code]
+        # args = [arg for arg in current_bounds.keys() if arg in expr_code]
         bounds = handle_angular_function(func_name, args, current_bounds)
         return bounds
     
@@ -111,14 +135,15 @@ def compute_expression_bounds_with_parsed_sensor(
         func = next(func for func in piecewise_functions if func.__name__ in expr_code)
         # TODO: handle args correctly, currently just passing everything which isn't correct, shouldn't be too difficult to extract the args from the piecewise function itself
         # TODO: Implement handling for piecewise functions
-        bounds = handle_piecewise_function(func, arg_names, current_bounds)
+        bounds = handle_piecewise_function(func, args, current_bounds)
         return bounds
+    
     # Check if the expression contains any map functions
     elif any(func in expr_code for func in MAP_FUNCTIONS):
         # Handle map functions
         func_name = [func for func in MAP_FUNCTIONS if func in expr_code][0]
-        args = [arg for arg in current_bounds.keys() if arg in expr_code]
-        bounds = handle_map_function(func_name, args, current_bounds)
+        # args = [arg for arg in current_bounds.keys() if arg in expr_code]
+        bounds = handle_map_function(func_name, args, current_bounds, track_map, track_mode)
         return bounds
 
     # Create the temporary function source code -- add nonce for caching to temp_func name
@@ -147,7 +172,8 @@ def handle_angular_function(func_name: str, args: List[str], current_bounds: Dic
     bounds_list = [current_bounds[name] for name in args]
     output_bounds = []
     
-    # Iterate over all possible combinations of bounds
+    # TODO(OPT): eventually this should be done in compute_expression_bounds once args are correctly extracted, do this once piecewise is handled
+    # Iterate over all possible combinations of bounds -- *bounds_list forms a set of list of tuples, and tuples are sampled out of each list
     for bounds_combination in itertools.product(*bounds_list):
         # Call the function with the current bounds combination
         try:
@@ -157,12 +183,13 @@ def handle_angular_function(func_name: str, args: List[str], current_bounds: Dic
             raise Exception(f"Error computing angular bounds: {e}")
     
     # Combine the output bounds using combine_angular_bounds
+    # TODO(OPT): pretty sure combine_angular_bounds function is way more complex than it needs to be -- why are positive and negative bounds being separately processed? they should be able to processed all at once
     combined_bounds = combine_angular_bounds(output_bounds)
     
     return combined_bounds
 
 
-def handle_map_function(func_name: str, args: List[str], current_bounds: Dict[str, List[Tuple[float, float]]]) -> Tuple[float, float]:
+def handle_map_function(func_name: str, args: List[str], current_bounds: Dict[str, List[Tuple[float, float]]], track_mode, track_map) -> Tuple[float, float]:
     """
     Handle map functions.
     
@@ -173,11 +200,29 @@ def handle_map_function(func_name: str, args: List[str], current_bounds: Dict[st
     
     Returns:
         (lower_bound, upper_bound) tuple
+    
+    NOTE: expecting map_function to be called of form map_function(x,y,...,z) and assuming alias will transform it to form map_function_real(track_map, track_mode, x, y, ...,z)
     """
 
     # TODO: Implement handling for map functions
     # TODO: add arguments for trackmode and map -- not sure if I want to do this for other functions yet
-    return (0.0, 1.0)
+    func = globals()[func_name]
+    # func = FUNC_DICT[func_name]
+    
+    # Call the function with the current bounds
+    bounds_list = [current_bounds[name] for name in args] # NOTE: is this guaranteed to be in the right order?
+    output_bounds = []
+
+    for bounds_combination in itertools.product(*bounds_list):
+        # Use parsed_sensor to compute bounds
+        try:
+            bounds = func(track_mode, track_map, *bounds_combination)
+            output_bounds.append(bounds)
+        except Exception as e:
+            raise Exception(f"Error computing map function bounds: {e}")
+    
+    # Return the minimum and maximum bounds
+    return [(min(np.array(output_bounds)[:,0]), max(np.array(output_bounds)[:,1]))] # NOTE: pretty sure this is correct, but could be wrong
 
 def handle_piecewise_function(func: Callable, args: List[str], current_bounds: Dict[str, List[Tuple[float, float]]]) -> Tuple[float, float]:
     """
@@ -189,14 +234,14 @@ def handle_piecewise_function(func: Callable, args: List[str], current_bounds: D
         current_bounds: Current variable bounds as a dict of str argument names -> tuple bounds 
     
     Returns:
-        (lower_bound, upper_bound) tuple
+        (lower_bound, upper_bound) tuple    
     """
     # TODO: implement this, preferably using parse_piecewise_function
     # NOTE: may have to move functions from bounded_piecewise to this file since I need to run some form of compute_expression_bounded on the outputs of the piecewise functions 
 
     return (0,1) 
 
-def handle_crown_function(func_code: str, args_names: List[str], current_bounds: Dict[str, List[Tuple[float, float]]]) -> Tuple[float, float]:
+def handle_crown_function(func_code: str, arg_names: List[str], current_bounds: Dict[str, List[Tuple[float, float]]]) -> Tuple[float, float]:
     """
     Handle CROWN functions.
     
@@ -214,10 +259,11 @@ def handle_crown_function(func_code: str, args_names: List[str], current_bounds:
     # Extract the arguments from the func_code
     args = []
     for node in ast.walk(func_def):
-        if isinstance(node, ast.Name) and node.id in args_names:
+        if isinstance(node, ast.Name) and node.id in arg_names:
             args.append(node.id)
 
-    # Remove duplicates and sort the arguments
+    # Remove duplicates and sort the arguments 
+    # NOTE: why sort? 
     args = sorted(list(set(args)))
     # NOTE: haven't thoroughly vetted this code yet, worst case, just set args = arg_names if unconfident it will work
 
@@ -253,30 +299,50 @@ def temp_func({', '.join(args)}):
     return [(min(lower_bounds), max(upper_bounds))]
 
 # Usage example:
+# NOTE: consider making this it's own file, don't really want to be testing stuff in this file
 if __name__ == "__main__":
-    def test_sensor(x, y):
-        z = x**2 + y**2
-        z = torch.sqrt(z) + torch.sum(torch.stack([x,y], dim=-1), dim=-1) # this is the correct way to do sums
-        # z = torch.sqrt(z) + x + y
-        w = arctan2(x, y)
-        return z # currently this does nothing, a return function just needs to exist
+    from verse.map import opendrive_map
+    from enum import Enum, auto
+    import os 
+
+    class TrackMode(Enum):
+        T0 = auto()
+        T1 = auto()
+        T2 = auto()
+        M01 = auto()
+        M12 = auto()
+        M21 = auto()
+        M10 = auto()
     
     def test_sensor(x, y):
-        z = x**2 + y**2
+        z = x**2 + y**2 # NOTE: CROWN just does this wrong apparently? min should be 0, not -0.5
+        # z_safe = torch.sqrt(z)
+        # xy_sum = torch.sum(torch.stack([x,y], dim=-1), dim=-1)
         w = arctan2(x,y)
-        l = w**2
-        # z = torch.sqrt(z) + torch.sum(torch.stack([x,y], dim=-1), dim=-1) # this is the correct way to do sums
+        m = arctan2(y,x) # FIXME: currently these two return the same output, which is wrong
+        # q = arctan2(x, w) # this works, but what this would represent in reality is unknown -- theoretically, w shouldn't be allowed as an input given this is from R^2 -> S1
+        # l = w**2
+        z = torch.sqrt(z) + torch.sum(torch.stack([x,y], dim=-1), dim=-1) # this is the correct way to do sums
         # # z = torch.sqrt(z) + x + y
         # w = arctan2(x, y)
         return z # currently this does nothing, a return function just needs to exist
     
+    def map_sensor(x,y):
+        z = get_lane_heading(x,y)
+        return z
+
     # TODO: add a way to automatically create the input bounds given a list of bounds and the function header -- user shouldn't need to do this by hand
     # input_bounds = {'x': (0, 1), 'y': (1, 2)}
     # input_bounds = {'x': [(0, 1)], 'y': [(1, 2), (2,3)]}
-    input_bounds = {'x': [(-1, -0.5)], 'y': [(-1, 0.5)]} # this will error out since I don't have a way to handle lists of tuples yet
+    # input_bounds = {'x': [(-1, -0.5)], 'y': [(-1, 0.5)]} 
+    input_bounds = {'x': [(134, 134.01)], 'y': [(11.5, 11.51)]} # TODO: check to see if this is consistent with the stanley scenario
     
 
-    final_bounds, history = parse_function(test_sensor, input_bounds)
+    script_dir = os.path.realpath(os.path.dirname(__file__))
+    tmp_map = opendrive_map(os.path.join(script_dir, "t1_triple.xodr"))
+
+    # final_bounds, history = parse_function(test_sensor, input_bounds)
+    final_bounds, history = parse_function(map_sensor, input_bounds, track_map=tmp_map, track_mode=TrackMode.T1)
     
     print("\nFinal bounds:")
     for var, bounds in final_bounds.items():
