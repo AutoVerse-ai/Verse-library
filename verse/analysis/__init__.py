7ccfe9c6ffb35b7449d8920d82a6825a629a1cc5
Alex Yuan <alexyuan0.ay@gmail.com>
2026-02-26 12:58:09 -0600

    comment out parsed_wrap import for now until I decide how to handle importing CROWN, may just use my fork like the Dockerfile does

--- verse/analysis/__init__.py
+++ verse/analysis/__init__.py
@@ -1,6 +1,6 @@
 from .analysis_tree import *
 from .simulator import Simulator
 from .verifier import Verifier, ReachabilityMethod
-from .parsed_wrap import clear_parse_cache, parse_function, parse_function_array
+# from .parsed_wrap import clear_parse_cache, parse_function, parse_function_array
 
 from . import simulator, verifier, analysis_tree
